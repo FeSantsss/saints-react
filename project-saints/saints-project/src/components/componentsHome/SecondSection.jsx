c3acf2d51864f38103040d83ddac95a9f5fd29f7
Felipy <tercetime18@gmail.com>
2026-03-16 19:16:13 -0300

    add news animations on images review

--- project-saints/saints-project/src/components/componentsHome/SecondSection.jsx
+++ project-saints/saints-project/src/components/componentsHome/SecondSection.jsx
@@ -1,8 +1,11 @@
 import '../../assets/secondSection.css'
+import {motion} from 'framer-motion'
+
 import aboutOne from '../../images/about-one.JPEG'
 import aboutTwo from '../../images/about-two.JPEG'
 import aboutThree from '../../images/about-three.JPEG'
 import aboutFour from '../../images/about-four.JPEG'
+
 
 const SecondSection = () => {
   return (
@@ -24,10 +27,86 @@ const SecondSection = () => {
         </h3>
 
         <div className="images-review">
-          <img src={aboutOne} className='image-review image-review-one' alt="image for review section" />
-          <img src={aboutTwo} className='image-review image-review-two' alt="image for review section" />
-          <img src={aboutThree} className='image-review image-review-three' alt="image for review section" />
-          <img src={aboutFour} className='image-review image-review-four' alt="image for review section" />
+
+          <motion.img 
+            src={aboutOne} 
+            className='image-review image-review-one' 
+            alt="image for review section" 
+            initial={{
+              opacity: 0,
+              y: -80,
+              filter: "blur(10px)"
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              filter: "blur(0px)"
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeOut"
+            }}
+            viewport={{ once: true }}
+          />
+
+          <motion.img src={aboutTwo} 
+            className='image-review image-review-two' 
+            alt="image for review section" 
+            initial={{
+              opacity: 0,
+              y: -80,
+              filter: "blur(10px)"
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              filter: "blur(0px)"
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeOut"
+            }}
+            viewport={{ once: true }}
+          />
+
+          <motion.img src={aboutThree} 
+            className='image-review image-review-three' 
+            alt="image for review section"
+            initial={{
+              opacity: 0,
+              y: -80,
+              filter: "blur(10px)"
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              filter: "blur(0px)"
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeOut"
+            }}
+            viewport={{ once: true }}
+          />
+          <motion.img src={aboutFour} 
+            className='image-review image-review-four'  
+            lt="image for review section" 
+            initial={{
+              opacity: 0,
+              y: -80,
+              filter: "blur(10px)"
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              filter: "blur(0px)"
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeOut"
+            }}
+            viewport={{ once: true }}
+          />
         </div>
 
         

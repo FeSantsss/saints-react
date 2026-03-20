5d9035ef2a29c656033fc18a8aacba3310a974fd
Felipy <tercetime18@gmail.com>
2026-03-19 23:39:04 -0300

    Error page modified

--- project-saints/saints-project/src/routes/ErrorPage.jsx
+++ project-saints/saints-project/src/routes/ErrorPage.jsx
@@ -1,4 +1,6 @@
 import {useRouteError} from 'react-router-dom'
+import '../assets/errorPage.css'
+import catError from '../images/cat-error.jpg'
 
 const ErrorPage = () => {
   const error = useRouteError();
@@ -6,7 +8,13 @@ const ErrorPage = () => {
   console.error(error);
 
   return (
-    <div>ErrorPage</div>
+    
+    <div className='div-error-page'>
+      <img src={catError} alt="image of cat for error page" />
+      <h1 className='title-error-page'>Oops!</h1>
+      <p className='about-error-page'>404 - Page Not Found</p>
+      <button className='error-page-btn'>Voltar para o Início</button>
+    </div>
   )
 }
 

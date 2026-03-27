ad7d1730aa0abf942ed9485aad70bc474952f81d
Felipy <tercetime18@gmail.com>
2026-03-27 19:24:01 -0300

    news: change on about page and animation on pages route

--- project-saints/saints-project/src/App.jsx
+++ project-saints/saints-project/src/App.jsx
@@ -1,23 +1,36 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Menu from './components/Menu'
-import Chat from './components/Chat'
-import Footer from './components/Footer'
-import ScrollBar from './components/ScrollBar'
+import { Outlet, useLocation } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/Menu";
+import Chat from "./components/Chat";
+import Footer from "./components/Footer";
+import ScrollBar from "./components/ScrollBar";
+
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
+  const location = useLocation();
 
   return (
     <>
-      <Menu/>
-      <ScrollBar/>
-      
-      <main><Outlet/></main>
-
-      <Chat/>
-      <Footer/>
+      <Menu />
+      <ScrollBar />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <main>
+            <Outlet />
+          </main>
+        </motion.div>
+      </AnimatePresence>
+      <Chat />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- project-saints/saints-project/src/routes/About.jsx
+++ project-saints/saints-project/src/routes/About.jsx
@@ -1,15 +1,33 @@
-
+import "../assets/about-page.css";
 
 const About = () => {
   return (
     <>
       <title>Saints Films | Sobre</title>
-      <div style={{height: "900px", textAlign: "center"}}>
-        <h1>About Page Test</h1>
-
+      <div className="section-about">
+        <h2>
+          <p className="title-about-page">somos a saints.</p>
+          <p className="text-about-page">
+            A Saints é uma produtora audiovisual orientada a posicionamento.
+          </p>
+          <p className="text-about-page">
+            Atuamos na construção estratégica de imagem para marcas, empresas e
+            profissionais que precisam consolidar presença com clareza e
+            direção.
+          </p>
+          <p className="text-about-page">
+            Com abordagem estética intencional, desenvolvemos filmes, coberturas
+            e narrativas visuais alinhadas a objetivos de percepção e
+            autoridade.
+          </p>
+          <p className="text-about-page">
+            Cada projeto é estruturado com consistência e propósito, para
+            transformar imagem em presença.
+          </p>
+        </h2>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
